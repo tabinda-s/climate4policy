f4fdce7b48fe145f252815380895a5939692c7a1
Ashlyn Kong <kong.ashlyn@gmail.com>
2019-01-13 20:26:10 -0500

    finish d3 charting, rearrange folder structure

--- ghg-gdp/scripts/viz.js
+++ ghg-gdp/scripts/viz.js
@@ -29,7 +29,8 @@ var valueline2 = d3.svg.line()
     .x(function(d) { return x(d.year); })
     .y(function(d) { return y1(d.co2); });
   
-var svg = d3.select("body")
+// Add svg canvas 1
+var svg1 = d3.select("#area1")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -52,24 +53,24 @@ d3.csv("ghg-gdp-ca.csv", function(error, data) {
     y1.domain([0, d3.max(data, function(d) { 
         return Math.max(d.co2); })]);
 
-    svg.append("path")        // Add the valueline path.
+    svg1.append("path")        // Add the valueline path.
         .attr("d", valueline(data));
 
-    svg.append("path")        // Add the valueline2 path.
+    svg1.append("path")        // Add the valueline2 path.
         .style("stroke", "red")
         .attr("d", valueline2(data));
 
-    svg.append("g")            // Add the X Axis
+    svg1.append("g")            // Add the X Axis
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    svg.append("g")
+    svg1.append("g")
         .attr("class", "y axis")
         .style("fill", "steelblue")
         .call(yAxisLeft);   
 
-    svg.append("text")       // left y axis label
+    svg1.append("text")       // left y axis label
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left)
       .attr("x",0 - (height / 2))
@@ -78,13 +79,13 @@ d3.csv("ghg-gdp-ca.csv", function(error, data) {
       .style("fill", "steelblue")
       .text("GDP per Capita");  
 
-    svg.append("g")             
+    svg1.append("g")             
         .attr("class", "y axis")    
         .attr("transform", "translate(" + width + " ,0)")   
         .style("fill", "red")       
         .call(yAxisRight);
 
-    svg.append("text")       // right y axis label
+    svg1.append("text")       // right y axis label
       .attr("transform", "rotate(-90)")
       .attr("y", margin.right)
       .attr("x", 0 - (height / 2))
@@ -93,4 +94,70 @@ d3.csv("ghg-gdp-ca.csv", function(error, data) {
       .style("fill", "red")
       .text("Carbon Dioxide Emissions");  
 
+});
+
+// Add svg canvas 2
+var svg2 = d3.select("#area2")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("ghg-gdp-wa.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.year = parseDate(d.year);
+        d.gdp = +d.gdp;
+        d.co2 = +d.co2;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.year; }));
+    y0.domain([0, d3.max(data, function(d) {
+        return Math.max(d.gdp); })]); 
+    y1.domain([0, d3.max(data, function(d) { 
+        return Math.max(d.co2); })]);
+
+    svg2.append("path")        // Add the valueline path.
+        .attr("d", valueline(data));
+
+    svg2.append("path")        // Add the valueline2 path.
+        .style("stroke", "red")
+        .attr("d", valueline2(data));
+
+    svg2.append("g")            // Add the X Axis
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg2.append("g")
+        .attr("class", "y axis")
+        .style("fill", "steelblue")
+        .call(yAxisLeft);   
+
+    svg2.append("text")       // left y axis label
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("fill", "steelblue")
+      .text("GDP per Capita");  
+
+    svg2.append("g")             
+        .attr("class", "y axis")    
+        .attr("transform", "translate(" + width + " ,0)")   
+        .style("fill", "red")       
+        .call(yAxisRight);
+
+    svg2.append("text")       // right y axis label
+      .attr("transform", "rotate(-90)")
+      .attr("y", margin.right)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "47em")
+      .style("text-anchor", "middle")
+      .style("fill", "red")
+      .text("Carbon Dioxide Emissions");  
 });
